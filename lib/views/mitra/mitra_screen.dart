76cddbf3eb3e9bca5801b4f9e60aad0bb4c3a5fe
Bagas Dwi Sulistyo <bag12dwi.s@gmail.com>
2024-10-25 08:06:47 +0700

    update terbaru

--- lib/views/mitra/mitra_screen.dart
+++ lib/views/mitra/mitra_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MitraScreen extends StatelessWidget {
   const MitraScreen({super.key});
@@ -8,6 +9,24 @@ class MitraScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Mitra"),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 100.h,
+              child: Image.asset('assets/mitra.png'),
+            ),
+            Text(
+              'Masa Depan Hukum',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
